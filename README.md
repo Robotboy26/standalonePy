aefb265a769f662d9c8ad172aef7d62195a6f125
robotboy26 <robotboygamer26@gmail.com>
2024-01-16 19:50:55 -0800

    Changes to be committed:

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # pyExe
+
+This is a very simple script that uses the pyinstaller package to create a standalone executable from a python file.
+
+Example:
+
+`
+./compile.sh <pythonFileName>
+`
---
+++ compile.sh
@@ -0,0 +1,11 @@
+# todo create a github actions that on push tests to make sure that the sample.py script (currently in gitignore) will compile to exe and run
+
+filename=$1
+filenameNoExt="${filename%%.*}"
+echo "$filenameNoExt"
+pip install pyinstaller
+pyinstaller $filename -F
+mv dist/$filenameNoExt ./
+rm -r build/ dist/
+rm $filenameNoExt.spec
+ls
